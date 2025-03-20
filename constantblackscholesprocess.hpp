52e6104693afaacc758c881c58c729dece20e645
yaslaa2002 <yasminelaarech@gmail.com>
2025-03-20 21:40:26 +0100

    First commit

--- constantblackscholesprocess.hpp
+++ constantblackscholesprocess.hpp
@@ -1,13 +1,28 @@
+//Authors : Yasmine LAARECH, Marie Camille MEMET, Amaury BERNARDIN , Hamid AMRANI
+
+#ifndef CONSTANT_BLACK_SCHOLES_PROCESS_HPP
+#define CONSTANT_BLACK_SCHOLES_PROCESS_HPP
 
 #include <ql/stochasticprocess.hpp>
 
 namespace QuantLib {
 
     class ConstantBlackScholesProcess : public StochasticProcess1D {
-
-        // your implementation goes here
+        public:
+            ConstantBlackScholesProcess(double x0, double dividendYield,double riskFreeRate, double volatility);
+            Real x0() const;
+            Real drift(Time t, Real x) const;
+            //Real evolve(Time t0, Real x0, Time dt, Real dw) const;
+            Real apply(Real x0, Real dx) const ;
+            Real diffusion(Time t, Real x) const;
+        private:
+            double x0_;  
+            double dividendYield_; 
+            double riskFreeRate_; 
+            double volatility_;   
         
     };
 
-}
+};
 
+#endif // CONSTANT_BLACK_SCHOLES_PROCESS_HPP
